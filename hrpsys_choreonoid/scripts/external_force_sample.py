f3ee4cc1775cec9b38e25376cd63d7532561beaf
YutaKojio <kojio@jsk.imi.i.u-tokyo.ac.jp>
2019-03-21 14:26:51 +0900

    add models

--- hrpsys_choreonoid/scripts/external_force_sample.py
+++ hrpsys_choreonoid/scripts/external_force_sample.py
@@ -1,7 +1,7 @@
 import cnoid.Base
 import cnoid.BodyPlugin
 
-def addExternalForce(fx=0,fy=0,fz=0,px=0,py=0,pz=0.2,t=0.2,link="WAIST",robot="JAXON_RED"):
+def addExternalForce(fx=0,fy=0,fz=0,px=0,py=0,pz=0,t=0.2,link="WAIST",robot="JAXON_RED"):
     robotname = robot
     linkname = link
     pos   = [px, py, pz] ## link local position
